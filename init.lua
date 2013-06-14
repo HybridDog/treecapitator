ff5a4cbe40cc9429131dd7866e98b7d70225bce7
HybridDog <adresse_test@t-online.de>
2013-06-15 00:18:00 +0200

    define trees in a better way

--- init.lua
+++ init.lua
@@ -1,11 +1,19 @@
 treecapitator = {}
 
+-----------------------------------------Old Settings------------------------------------------
+--local	timber_nodenames = {"default:jungletree", "default:tree", "sumpf:tree"}
+--local	leaves_nodenames = {"default:leaves", "default:jungleleaves", "sumpf:leaves"}
+--local	fruit_nodenames  = {"default:apple", "sumpf:tree_horizontal"}
+--local	size = 2	--2*size+1
+
+
 -------------------------------------------Settings--------------------------------------------
 treecapitator.drop_items = true	--drop them / get them in the inventory
-local	timber_nodenames = {"default:jungletree", "default:tree", "sumpf:tree"}
-local	leaves_nodenames = {"default:leaves", "default:jungleleaves", "sumpf:leaves"}
-local	fruit_nodenames  = {"default:apple", "sumpf:tree_horizontal"}
-local	size = 2	--2*size+1
+local trees_to_capitate = {--	trees	leaves	range	fruits
+	{{"default:jungletree"}, {"default:jungleleaves"}, 3, {}},
+	{{"default:tree"}, {"default:leaves"}, 2, {"default:apple"}},
+	{{"sumpf:tree", "sumpf:mossytree"}, {"sumpf:leaves"}, 3, {"sumpf:tree_horizontal"}},
+}
 -----------------------------------------------------------------------------------------------
 
 local function dropitem(item, posi, digger)
@@ -19,44 +27,79 @@ local function dropitem(item, posi, digger)
 	minetest.env:add_item(posi, item)
 end
 
+local function gettree(trees, pos)
+	for _, tree in ipairs(trees) do
+		if minetest.env:get_node(pos).name == tree then
+			return true
+		end
+	end
+	return false
+end
+
+local function findtree(pos, node)
+	for tr in ipairs(trees_to_capitate) do
+		for _, tree in ipairs(trees_to_capitate[tr][1]) do
+			if node.name == tree then
+				return true
+			end
+		end
+	end
+	return false
+end
+
+
+
 minetest.register_on_dignode(function(pos, node, digger)
-    if digger:get_player_control().sneak then
+	if digger == nil then
 		return
 	end
-	local i=1
-	while timber_nodenames[i]~=nil do	--trunk stuff
-		if node.name==timber_nodenames[i] then
-			np={x=pos.x, y=pos.y+1, z=pos.z}
-			while minetest.env:get_node(np).name==timber_nodenames[i] do
-				minetest.env:remove_node(np)
-				dropitem(timber_nodenames[i], np, digger)
-				np.y = np.y+1
-			end
-			for i=-size,size,1 do	--definition of the leavesposition
-				for j=-size,size,1 do
-					for k=-size,size,1 do
-					p={x=np.x+i, y=np.y-1+j, z=np.z+k}
-						for _, leaves in ipairs(leaves_nodenames) do	--definition of the leaves
-							if minetest.env:get_node(p).name==leaves then
-								itemstacks = minetest.get_node_drops(minetest.env:get_node(p).name)
-								for _, itemname in ipairs(itemstacks) do
-									if minetest.env:get_node(p).name ~= itemname then
-										dropitem(itemname, p, digger)
-									end
+    if digger:get_player_control().sneak
+    or (not findtree(pos, node)) then
+		return
+	end
+	local np = {x=pos.x, y=pos.y+1, z=pos.z}
+	for tr in ipairs(trees_to_capitate) do
+		while gettree(trees_to_capitate[tr][1], np) do
+			local tree = minetest.env:get_node(np).name
+			minetest.env:remove_node(np)
+			dropitem(tree, np, digger)
+			np.y = np.y+1
+		end
+		local leaves = trees_to_capitate[tr][2]
+		local range = trees_to_capitate[tr][3]
+		local fruits = trees_to_capitate[tr][4]
+		for i = -range,range,1 do	--definition of the leavesposition
+			for j = -range-1,range-1,1 do
+				for k = -range,range,1 do
+					p = {x=np.x+i, y=np.y+j, z=np.z+k}
+					nodename = minetest.env:get_node(p).name
+					local foundnode = false
+					for _, leaf in ipairs(leaves) do
+						if nodename == leaf then
+							local leaves_drops = minetest.get_node_drops(leaf)
+							for _, itemname in ipairs(leaves_drops) do
+								if itemname ~= leaf then
+									dropitem(itemname, p, digger)
 								end
-								minetest.env:remove_node(p)	--remove the leaves
 							end
+							minetest.env:remove_node(p)	--remove the leaves
+							foundnode = true
+							break
 						end
-						for _, fruit in ipairs(fruit_nodenames) do	--definition of the fruits
-							if minetest.env:get_node(p).name==fruit then
+					end
+					if not foundnode then
+						for _, fruit in ipairs(fruits) do
+							if nodename == fruit then
 								dropitem(fruit, p, digger)
 								minetest.env:remove_node(p)	--remove the fruit
+								break
 							end
 						end
 					end
 				end
 			end
 		end
-		i=i+1
 	end
 end)
+
+print("[treecapitator] loaded")
