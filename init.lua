2fd111e6e5898a714858e29f75d594651bb15732
HybridDog <adresse_test@t-online.de>
2013-08-24 12:41:01 +0200

    update

--- init.lua
+++ init.lua
@@ -13,6 +13,7 @@ local trees_to_capitate = {--	trees	leaves	range	fruits
 	{{"default:jungletree"}, {"default:jungleleaves"}, 3, {}},
 	{{"default:tree"}, {"default:leaves"}, 2, {"default:apple"}},
 	{{"sumpf:tree", "sumpf:mossytree"}, {"sumpf:leaves"}, 3, {"sumpf:tree_horizontal"}},
+	{{"nyanland:mesetree", "nyanland:healstone"}, {"nyanland:meseleaves"}, 2, {"default:apple"}},
 }
 -----------------------------------------------------------------------------------------------
 
@@ -47,6 +48,58 @@ local function findtree(pos, node)
 	return false
 end
 
+local function find_near_tree(pos, range, trees)
+	--[[for a = -1,1,2 do
+		for i = 1,range*2,1 do
+			for l = -i+1, i, 1 do
+				m = {x=pos.x+i*a, y=pos.y, z=pos.z-l*a}
+				n = {x=pos.x+l*a, y=pos.y, z=pos.z+i*a}
+				for tr in ipairs(trees) do
+					if minetest.env:get_node(m).name == tr then
+						print(m.x)
+						return m
+					end
+					if minetest.env:get_node(n).name == tr then
+						print(n.x)
+						return n
+					end
+				end
+			end
+		end
+	end]]
+	for i = -range*2,range*2,1 do
+		for j = -range*2,range*2,1 do
+			m = {x=pos.x+i, y=pos.y, z=pos.z+j}
+			for tr in ipairs(trees) do
+				if minetest.env:get_node(m).name == tr then
+					print(m.x)
+					return m
+				end
+			end
+		end
+	end
+	return pos
+end
+
+local function change_range(pos, range, trees)
+	p = find_near_tree(pos, range, trees)
+	if p == pos then
+		return range
+	end
+	ap_x = math.abs(p.x)
+	if math.max(ap_x, math.abs(p.z)) == ap_x then
+		c = p.x
+	else
+		c = p.z
+	end
+	f = c - range
+	g = math.abs(c) - range
+	if g <= -1 then
+		return 0
+	end
+	return f
+end
+
 
 
 minetest.register_on_dignode(function(pos, node, digger)
@@ -66,6 +119,7 @@ minetest.register_on_dignode(function(pos, node, digger)
 			np.y = np.y+1
 		end
 		local leaves = trees_to_capitate[tr][2]
+--		local range = change_range(pos, trees_to_capitate[tr][3], trees_to_capitate[tr][1])
 		local range = trees_to_capitate[tr][3]
 		local fruits = trees_to_capitate[tr][4]
 		for i = -range,range,1 do	--definition of the leavesposition
