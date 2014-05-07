2c2d8fe3451d2e2eae2f0a48fe284a34ca824933
HybridDog <adresse_test@t-online.de>
2014-05-07 20:08:53 +0200

    only capitate trees if a leaf (or fruit because of these apple trees...)is about the tree node and change other stuff

--- init.lua
+++ init.lua
@@ -5,35 +5,27 @@ treecapitator = {}
 --------------------------------------Setting-----------------------------------------------
 
 treecapitator.drop_items = false	--drop them / get them in the inventory
+treecapitator.drop_leaf = false
 
 ---------------------------------------------------------------------------------------------
 
 
 treecapitator.trees = {}
-treecapitator.num = 1
+local num = 1
 
  --replaces not defined stuff
 local function set_tree_defaults(tab)
-	if not tab.trees then
-		tab.trees = {"default:tree"}
-	end
-	if not tab.leaves then
-		tab.leaves = {"default:leaves"}
-	end
-	if not tab.range then
-		tab.range = 2
-	end
-	if not tab.fruits then
-		tab.fruits = {}
-	end
+	tab.trees = tab.trees or {"default:tree"}
+	tab.leaves = tab.leaves or {"default:leaves"}
+	tab.range = tab.range or 2
+	tab.fruits = tab.fruits or {}
 	return tab
 end
 
 function treecapitator.register_tree(tab)
 	tab = set_tree_defaults(tab)
-	local num = treecapitator.num
 	treecapitator.trees[num] = tab
-	treecapitator.num = num+1
+	num = num+1
 end
 
 dofile(minetest.get_modpath("treecapitator").."/trees.lua")
@@ -41,13 +33,13 @@ dofile(minetest.get_modpath("treecapitator").."/trees.lua")
 
 --------------------------------------------fcts----------------------------------------------
 
-
+local destroy_node, drop_leaf, remove_leaf
 if treecapitator.drop_items then
-	function treecapitator.drop_leaf(pos, item, inv)
+	function drop_leaf(pos, item, inv)
 		minetest.add_item(pos, item)
 	end
 
-	function treecapitator.destroy_node(pos, node, digger)
+	function destroy_node(pos, node, digger)
 		local drops = minetest.get_node_drops(node.name)
 		for _,item in ipairs(drops) do
 			minetest.add_item(pos, item)
@@ -55,7 +47,7 @@ if treecapitator.drop_items then
 		minetest.remove_node(pos)
 	end
 else
-	function treecapitator.drop_leaf(pos, item, inv)
+	function drop_leaf(pos, item, inv)
 		if inv
 		and inv:room_for_item("main", item) then
 			inv:add_item("main", item)
@@ -64,13 +56,28 @@ else
 		end
 	end
 
-	function treecapitator.destroy_node(pos, node, digger)
+	function destroy_node(pos, node, digger)
 		minetest.node_dig(pos, node, digger)
 	end
 end
 
+if not treecapitator.drop_leaf then
+	function remove_leaf(p, leaf, inv)
+		local leaves_drops = minetest.get_node_drops(leaf)
+		for _, itemname in ipairs(leaves_drops) do
+			if itemname ~= leaf then
+				drop_leaf(p, itemname, inv)
+			end
+		end
+		minetest.remove_node(p)	--remove the leaves
+	end
+else
+	function remove_leaf(p, _, _, node, digger)
+		destroy_node(p, node, digger)
+	end
+end
 
-local function table_contains(t, v)
+table.icontains = table.icontains or function(t, v)
 	for _,i in ipairs(t) do
 		if i == v then
 			return true
@@ -81,7 +88,7 @@ end
 
 local function findtree(nodename)
 	for _,tr in ipairs(treecapitator.trees) do
-		if table_contains(tr.trees, nodename) then
+		if table.icontains(tr.trees, nodename) then
 			return true
 		end
 	end
@@ -94,47 +101,56 @@ minetest.register_on_dignode(function(pos, node, digger)
 		return
 	end
     if digger:get_player_control().sneak
-    or (not findtree(node.name)) then
+    or not findtree(node.name) then
 		return
 	end
 	local t1 = os.clock()
 	local np = {x=pos.x, y=pos.y+1, z=pos.z}
 	for _,tr in ipairs(treecapitator.trees) do
-		while table_contains(tr.trees, minetest.get_node(np).name) do
-			local tree = minetest.get_node(np)
-			treecapitator.destroy_node(np, tree, digger)
-			np.y = np.y+1
-		end
-		local leaves = tr.leaves
---		local range = change_range(pos, tr.range, tr.trees)
-		local range = tr.range
-		local fruits = tr.fruits
-		local inv = digger:get_inventory()
-		for i = -range,range,1 do	--definition of the leavesposition
-			for j = -range-1,range-1,1 do
-				for k = -range,range,1 do
-					p = {x=np.x+i, y=np.y+j, z=np.z+k}
-					node = minetest.get_node(p)
-					nodename = node.name
-					local foundnode = false
-					for _, leaf in ipairs(leaves) do
-						if nodename == leaf then
-							local leaves_drops = minetest.get_node_drops(leaf)
-							for _, itemname in ipairs(leaves_drops) do
-								if itemname ~= leaf then
-									treecapitator.drop_leaf(p, itemname, inv)
-								end
-							end
-							minetest.remove_node(p)	--remove the leaves
-							foundnode = true
-							break
-						end
-					end
-					if not foundnode then
-						for _,fruit in ipairs(fruits) do
-							if nodename == fruit then
-								treecapitator.destroy_node(p, node, digger) --remove the fruit
+		local nd = minetest.get_node(np)
+		local trees = tr.trees
+		local tree_found = table.icontains(trees, nd.name)
+		if tree_found then
+			local tab, n = {}, 1
+			while tree_found do
+				tab[n] = {vector.new(np), nd}
+				n = n+1
+				np.y = np.y+1
+				nd = minetest.get_node(np)
+				tree_found = table.icontains(trees, nd.name)
+			end
+			local leaves = tr.leaves
+			local fruits = tr.fruits
+			if not table.icontains(leaves, nd.name)
+			and not table.icontains(fruits, nd.name) then
+				return
+			end
+			for _,i in ipairs(tab) do
+				destroy_node(i[1], i[2], digger)
+			end
+--			local range = change_range(pos, tr.range, tr.trees)
+			local range = tr.range
+			local inv = digger:get_inventory()
+			for i = -range, range do	--definition of the leavesposition
+				for j = -range-1, range-1 do
+					for k = -range, range do
+						local p = {x=np.x+i, y=np.y+j, z=np.z+k}
+						local node = minetest.get_node(p)
+						local nodename = node.name
+						local foundnode = false
+						for _, leaf in ipairs(leaves) do
+							if nodename == leaf then
+								remove_leaf(p, leaf, inv, node, digger)
+								foundnode = true
 								break
+							end
+						end
+						if not foundnode then
+							for _,fruit in ipairs(fruits) do
+								if nodename == fruit then
+									destroy_node(p, node, digger) --remove the fruit
+									break
+								end
 							end
 						end
 					end
